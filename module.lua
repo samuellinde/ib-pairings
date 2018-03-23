99f46d378dbbda3adea8ed02b3b6a0339e90c72c
Samuel Linde <samuel@dontblink.se>
2018-03-23 23:14:59 +0100

    text

--- module.lua
+++ module.lua
@@ -4,15 +4,18 @@ local M = {}
 
 local font = resource.load_font(localized "Roboto-Medium.ttf")
 local bg_image = resource.load_image(localized "redwedding.jpg")
+local texture = resource.create_colored_texture(0, 0, 0, 0.8)
+
 local font_size = 60
 -- local font_y = 200
-local pairings = 'yada'
+local pairings
 
 print "sub module init"
 
 function M.draw()
   gl.clear(0, 0, 0, 1)
   bg_image:draw(0, 0, WIDTH, HEIGHT)
+  texture:draw(0, 0, WIDTH, HEIGHT)
   local text_width = font:width(pairings, font_size)
   local font_x = WIDTH / 2 - text_width / 2
   local font_y = HEIGHT / 2 - font_size / 2
