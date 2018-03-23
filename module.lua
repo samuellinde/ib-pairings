072ec407783cd76a8bb7b8131d7c4beba6b46ffe
Samuel Linde <samuel@dontblink.se>
2018-03-23 20:58:56 +0100

    fix

--- module.lua
+++ module.lua
@@ -4,15 +4,14 @@ local M = {}
 
 local font = resource.load_font(localized "Roboto-Medium.ttf")
 local font_size = 60
-local font_x
 local text = ''
 
 print "sub module init"
 
 local function draw_text(t)
   local text_width = font:width(t, font_size)
-  font_x = DEVICE_WIDTH / 2 - text_width / 2
-  font_y = DEVICE_HEIGHT / 2 - font_size * 1.5
+  local font_x = DEVICE_WIDTH / 2 - text_width / 2
+  local font_y = DEVICE_HEIGHT / 2 - font_size * 1.5
   font:write(font_x, font_y, text, font_size, 1,1,1,1)
 end
 
@@ -26,8 +25,10 @@ end
 
 function M.content_update(name)
   print("sub module content update", name)
-  if name == 'wer.txt' then
-    text = resource.load_file(localized(name))
+  if name == 'config.json' then
+    json_file = resource.load_file(localized(name))
+    config = json.decode(json_file)
+    text = resource.load_file(localized(config.text))
   end
 end
 
